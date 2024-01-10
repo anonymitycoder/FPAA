8bf4261f628a1f4bc07805d75dfcb822c8caaf6e
anonymitycoder <149573883+anonymitycoder@users.noreply.github.com>
2024-01-10 17:44:35 +0800

    Add files via upload

--- get_threshold.py
+++ get_threshold.py
@@ -1,7 +1,10 @@
+import argparse
+
 import librosa
 import numpy as np
 import torch
 from scipy import signal
+import os
 
 
 def compute_PSD_matrix(stft, window_size):
@@ -26,11 +29,11 @@ def quiet(f):
 
 def two_slops(bark_psd, delta_TM, bark_maskee):
     Ts = []
-    for tone_mask in range(bark_psd.size(0)):  # Use size() for PyTorch tensors
+    for tone_mask in range(bark_psd.size(0)):
         bark_masker = bark_psd[tone_mask, 0]
         dz = bark_maskee - bark_masker
         zero_index = torch.argmax((dz > 0).float())
-        sf = torch.zeros_like(dz)  # Use torch.zeros_like for PyTorch tensors
+        sf = torch.zeros_like(dz)
         sf[:zero_index] = 27 * dz[:zero_index]
         sf[zero_index:] = (-27 + 0.37 * torch.maximum(bark_psd[tone_mask, 1] - 40, torch.tensor(0.0))) * dz[zero_index:]
         T = bark_psd[tone_mask, 1] + delta_TM[tone_mask] + sf
@@ -39,29 +42,23 @@ def two_slops(bark_psd, delta_TM, bark_maskee):
 
 
 def compute_th(PSD, barks, ATH, freqs):
-    # Identification of tonal maskers
-    # find the index of maskers that are the local maxima
     length = PSD.size(0)
-    masker_index = torch.tensor(signal.argrelextrema(PSD.cpu().numpy(), np.greater)[0])  # Using SciPy for local maxima
+    masker_index = torch.tensor(signal.argrelextrema(PSD.cpu().numpy(), np.greater)[0])
 
-    # Delete the boundary of maskers for smoothing
     masker_index = masker_index[masker_index != 0]
     masker_index = masker_index[masker_index != length - 1]
     num_local_max = masker_index.size(0)
 
-    # Smooth the PSD
     p_k = torch.pow(10, PSD[masker_index] / 10.)
     p_k_prev = torch.pow(10, PSD[masker_index - 1] / 10.)
     p_k_post = torch.pow(10, PSD[masker_index + 1] / 10.)
     P_TM = 10 * torch.log10(p_k_prev + p_k + p_k_post)
 
-    # Prepare bark_psd tensor
     bark_psd = torch.zeros(num_local_max, 3)
     bark_psd[:, 0] = barks[masker_index]
     bark_psd[:, 1] = P_TM
     bark_psd[:, 2] = masker_index.float()
 
-    # Delete the maskers not having the highest PSD within 0.5 Bark around their frequency
     i = 0
     while i < bark_psd.size(0):
         next_i = i + 1
@@ -78,37 +75,71 @@ def compute_th(PSD, barks, ATH, freqs):
 
         i += 1
 
-    # Compute the individual masking threshold
     delta_TM = 1 * (-6.025 - 0.275 * bark_psd[:, 0])
-    Ts = two_slops(bark_psd, delta_TM, barks)  # Make sure two_slopes is also converted to PyTorch
+    Ts = two_slops(bark_psd, delta_TM, barks)
 
     if not Ts:
         Ts = torch.zeros(len(barks))
     else:
         Ts = torch.stack(Ts)
 
-    # Compute the global masking threshold
     theta_x = torch.sum(torch.pow(10, Ts / 10.), dim=0) + torch.pow(10, ATH / 10.)
 
     return theta_x
 
 
 def generate_th(stft, fs, window_size):
-    PSD, psd_max = compute_PSD_matrix(stft=stft, window_size=2048)  # Ensure this is the PyTorch version
+    PSD, psd_max = compute_PSD_matrix(stft=stft, window_size=2048)
 
     freqs = librosa.core.fft_frequencies(sr=fs, n_fft=window_size)
-    barks = Bark(torch.tensor(freqs))  # Bark function in PyTorch
+    barks = Bark(torch.tensor(freqs))
 
-    # Compute the quiet threshold
     ATH = torch.full((len(barks),), float('-inf'))
     bark_ind = torch.argmax((barks > 1).float())
-    ATH[bark_ind:] = quiet(torch.tensor(freqs[bark_ind:]))  # quiet function in PyTorch
+    ATH[bark_ind:] = quiet(torch.tensor(freqs[bark_ind:]))
 
-    # Compute the global masking threshold theta_xs
     theta_xs = []
-    # Compute the global masking threshold in each window
     for i in range(PSD.shape[1]):
-        theta_xs.append(compute_th(PSD[:, i], barks, ATH, torch.tensor(freqs)))  # compute_th in PyTorch
+        theta_xs.append(compute_th(PSD[:, i], barks, ATH, torch.tensor(freqs)))
     theta_xs = torch.stack(theta_xs)
 
     return theta_xs, psd_max
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, default='urban8K', help='Dataset name')
+    parser.add_argument('--psd_save_path', type=str, default='urban_psd', help='Path to save PSD')
+    parser.add_argument('--sample_rate', type=int, default=22050, help='Sample rate')
+    parser.add_argument('--window_size', type=int, default=2048, help='Window size')
+    args = parser.parse_args()
+
+    psd_path = 'psd'
+
+    if args.dataset == 'urban8K':
+        batch_size = 50
+        n_class = 10
+        data_path = 'urban8k_data'
+        save_path = psd_path + 'urban_psd'
+    else:
+        batch_size = 10
+        n_class = 50
+        data_path = 'esc_data'
+        save_path = psd_path + 'esc_psd'
+
+    for class_id in range(n_class):
+        th_batch = []
+        psd_max_batch = []
+        data = torch.load(os.path.join(data_path, f'class{class_id}', 'real', 'correct_real.pt'))
+        for i in range(batch_size):
+            th, psd_max = generate_th(data[i].squeeze(), args.sample_rate, args.window_size)
+            th_batch.append(th)
+            psd_max_batch.append(psd_max)
+        th_batch = torch.stack(th_batch)
+        psd_max_batch = torch.stack(psd_max_batch)
+
+        path = os.path.join(save_path, f'class{class_id}')
+        os.makedirs(path, exist_ok=True)
+
+        torch.save(th_batch, os.path.join(path, 'th_batch.pt'))
+        torch.save(psd_max_batch, os.path.join(path, 'psd_max_batch.pt'))
