241f812ed449b5403216a0e2d8c2914787f2bad0
anonymitycoder <149573883+anonymitycoder@users.noreply.github.com>
2024-04-19 14:54:32 +0800

    Update Autoattack.py

--- Baseline/AutoAttack/Autoattack.py
+++ Baseline/AutoAttack/Autoattack.py
@@ -37,8 +37,7 @@ def load_model(model_name, num_classes):
 
 
 def main(args):
-    model_names = [
-        'DenseNet']
+    model_names = ['VGG13','VGG16','DenseNet','GoogLeNet','SB_CNN']
     for model_name in model_names:
         print('#################################')
         print(f"Processing model: {model_name}")
@@ -48,19 +47,17 @@ def main(args):
 
         model = load_model(model_name, num_classes=num_classes)
 
-        # adversary = AutoAttack(model, norm='Linf', eps=args.epsilon, version='standard')
-        adversary = APGDAttack(model, n_restarts=1, n_iter=10, verbose=True,
-                               eps=args.epsilon, norm='Linf', eot_iter=1, rho=.75,seed=1,loss='ce')
+        adversary = AutoAttack(model, norm='Linf', eps=args.epsilon, version='standard')
 
         save_path = Path(args.save_path) / args.dataset / model_name / args.attack_method
         save_path.mkdir(parents=True, exist_ok=True)
 
         for class_id in range(num_classes):
             print(f'The current class under attack is the {class_id}th class.')
-            data = torch.load(data_path / f'class{class_id}' / 'real' / 'correct_real.pt')[0:25]
-            label = torch.load(data_path / f'class{class_id}' / 'label' / 'correct_labels.pt')[0:25]
+            data = torch.load(data_path / f'class{class_id}' / 'real' / 'correct_real.pt')
+            label = torch.load(data_path / f'class{class_id}' / 'label' / 'correct_labels.pt')
 
-            x_adv = adversary.perturb(data, label)
+            x_adv = adversary.run_standard_evaluation(data, label)
 
             torch.save(x_adv, save_path / f'class_{class_id}_adv_audio.pt')
 
